9479e6a44aea6fb1c6b6831ec9c02b3a622a7958
Husaria55 <krikbart@gmail.com>
2026-02-15 16:45:48 +0100

    Nearly done, only dumping ratio

--- excel_sheet_functions.py
+++ excel_sheet_functions.py
@@ -1,5 +1,7 @@
 from rocketpy import Flight, LiquidMotor, Rocket
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def rail_departure_velocity_in_ft_per_sec(flight: Flight) -> float:
     """
@@ -155,6 +157,22 @@ def max_acceleration_in_g(flight: Flight) -> tuple:
     return max_acceleration_in_g, max_acceleration_time
 
 
+def max_acceleration_power_on_in_g(flight: Flight) -> tuple:
+    """
+    Calculate the maximum acceleration during the powered flight in g's.
+
+    Parameters:
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    tuple: The maximum acceleration during the powered flight in g's and the time at which it occurs in seconds.
+    """
+    max_acceleration_power_on = flight.max_acceleration_power_on
+    max_acceleration_power_on_time = flight.max_acceleration_power_on_time
+    max_acceleration_power_on_in_g = max_acceleration_power_on / 9.80665
+    return max_acceleration_power_on_in_g, max_acceleration_power_on_time
+
+
 def max_velocity_in_ft_per_sec(flight: Flight) -> tuple:
     """
     Calculate the maximum velocity during the flight in feet per second.
@@ -171,7 +189,7 @@ def max_velocity_in_ft_per_sec(flight: Flight) -> tuple:
     return max_speed_ft_s, max_speed_time   
 
 
-def max_q_in_psi_and_altitude_in_ft(flight: Flight) -> tuple:
+def max_q_in_psf_and_altitude_in_ft(flight: Flight) -> tuple:
     """
     Calculate the maximum dynamic pressure during the flight in psi and the altitude at which it occurs in feet.
 
@@ -179,13 +197,29 @@ def max_q_in_psi_and_altitude_in_ft(flight: Flight) -> tuple:
     flight (Flight): The Flight object containing the flight data.
 
     Returns:
-    tuple: The maximum dynamic pressure during the flight in psi and the time at which it occurs in seconds and the altitude at which it occurs in feet.
+    tuple: The maximum dynamic pressure during the flight in psf and the time at which it occurs in seconds and the altitude at which it occurs in feet.
     """
     max_dynamic_pressure = flight.max_dynamic_pressure
     max_dynamic_pressure_time = flight.max_dynamic_pressure_time
     altitude_at_max_pressure = flight.z(max_dynamic_pressure_time) * 3.28084
-    max_dynamic_pressure_psi = max_dynamic_pressure * 0.000145038
-    return max_dynamic_pressure_psi, max_dynamic_pressure_time, altitude_at_max_pressure
+    max_dynamic_pressure_psf = max_dynamic_pressure * 0.0208854
+    return max_dynamic_pressure_psf, max_dynamic_pressure_time, altitude_at_max_pressure
+
+
+def max_altitude_in_ft_and_time(flight: Flight) -> tuple:
+    """
+    Calculate the maximum altitude during the flight in feet and the time at which it occurs in seconds.
+
+    Parameters:
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    tuple: The maximum altitude during the flight in feet and the time at which it occurs in seconds.
+    """
+    max_altitude = flight.apogee
+    max_altitude_time = flight.apogee_time
+    max_altitude_ft = max_altitude * 3.28084
+    return max_altitude_ft, max_altitude_time
 
 
 def distance_from_pad(flight: Flight) -> float:
@@ -240,3 +274,169 @@ def max_pitch_moment(flight: Flight) -> tuple:
     return M2_max, M2_max_time
 
 
+def get_df_for_mach_number(flight: Flight) -> pd.DataFrame:
+    """
+    Get the Mach number data as a DataFrame.
+
+    Parameters:
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    dataframe: A DataFrame containing the time and Mach number data.
+    """
+    mach_number = flight.mach_number
+    mach_number_array = np.array(mach_number)
+    mach_df = pd.DataFrame(mach_number_array, columns=['Time', 'Mach Number'])
+    return mach_df
+
+
+def get_df_for_center_of_pressure(rocket: Rocket, flight: Flight) -> pd.DataFrame:
+    """
+    Get the center of pressure data as a DataFrame.
+
+    Parameters:
+    rocket (Rocket): The Rocket object containing the rocket data.
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    dataframe: A DataFrame containing the Mach number and center of pressure data.
+    """
+    max_mach_number = flight.max_mach_number
+    cp = rocket.evaluate_center_of_pressure()
+    cp.savetxt('center_of_pressure.csv', lower=0, upper=max_mach_number, samples=1000)
+    cp_df = pd.read_csv('center_of_pressure.csv') # center of pressure dataframe
+    return cp_df
+
+
+def get_df_for_angle_of_attack(flight: Flight) -> pd.DataFrame:
+    """
+    Get the angle of attack data as a DataFrame.
+
+    Parameters:
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    dataframe: A DataFrame containing the time and angle of attack data.
+    """
+    aoa = flight.angle_of_attack
+    aoa.savetxt('angle_of_attack.csv', samples=1000, encoding='utf-8')
+    aoa_df = pd.read_csv('angle_of_attack.csv') # angle of attack dataframe
+    return aoa_df
+
+
+def get_df_for_center_of_gravity(rocket: Rocket, flight: Flight) -> pd.DataFrame:
+    """
+    Get the center of gravity data as a DataFrame.
+
+    Parameters:
+    rocket (Rocket): The Rocket object containing the rocket data.
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    dataframe: A DataFrame containing the time and center of gravity data.
+    """
+    cog = rocket.evaluate_center_of_mass()
+    cog.savetxt('center_of_gravity.csv', samples=1000)
+    cog_df = pd.read_csv('center_of_gravity.csv') # center of gravity dataframe
+    return cog_df
+ 
+
+def get_merged_df_for_cp_and_mach_number(rocket: Rocket, flight: Flight) -> pd.DataFrame:
+    """
+    Get a merged DataFrame for center of pressure and Mach number.
+
+    Parameters:
+    rocket (Rocket): The Rocket object containing the rocket data.
+    flight (Flight): The Flight object containing the flight data.
+
+    Returns:
+    dataframe: A merged DataFrame containing the Mach number and center of pressure data.
+    """
+    mach_df = get_df_for_mach_number(flight)
+    cp_df = get_df_for_center_of_pressure(rocket, flight)
+    max_mach_number_index = mach_df['Mach Number'].idxmax()
+    mach_to_max_df = mach_df.loc[:max_mach_number_index]
+    cp_df = cp_df.rename(columns={'x': 'Mach_Ref'}) 
+    cp_df = cp_df.sort_values('Mach_Ref')
+    mach_to_max_df = mach_to_max_df.sort_values('Mach Number')
+    merged_df = pd.merge_asof(
+        cp_df, 
+        mach_to_max_df, 
+        left_on='Mach_Ref', 
+        right_on='Mach Number', 
+        direction='nearest'
+    )
+    return merged_df
+
+
+def plot_center_of_pressure_vs_mach_number(cop_df: pd.DataFrame):
+    """
+    Plot the center of pressure position vs Mach number.
+
+    Parameters:
+    cop_df (pd.DataFrame): A DataFrame containing the Mach number and center of pressure data.
+    """
+    x_mach = np.array(cop_df["x"])
+    y_cop = np.array(cop_df["Scalar"])
+    font = {'family': 'serif',
+        'color':  'black',
+        'size': 12}
+    plt.plot(x_mach, y_cop)
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 1)
+    plt.xlabel("Mach number", fontdict=font)
+    plt.ylabel("Center of pressure position (m)", fontdict=font)
+    plt.title("Center of pressure position vs Mach number", fontdict=font)
+    plt.show()
+
+
+def plot_center_of_gravity_and_of_pressure_vs_time(cog_df: pd.DataFrame, merged_df: pd.DataFrame):
+    """
+    Plot the center of gravity and center of pressure position vs time.
+
+    Parameters:
+    cog_df (pd.DataFrame): A DataFrame containing the time and center of gravity data.
+    merged_df (pd.DataFrame): A DataFrame containing the time and center of pressure data.
+    """
+    font = {'family': 'serif',
+        'color':  'black',
+        'size': 12}
+    y2_cog = np.array(cog_df["Center of Mass Position (m)"])
+    merged_cp_and_mach_df_reshaped = merged_df.groupby(merged_df.index // 10).mean()
+    x_time = np.array(merged_cp_and_mach_df_reshaped["Time"])
+    y_cop = np.array(merged_cp_and_mach_df_reshaped["Scalar"])
+    plt.plot(x_time, y_cop)
+    plt.plot(x_time, y2_cog)
+    plt.xlabel("Time (s)", fontdict=font)
+    plt.ylabel("Position (m)", fontdict=font)
+    plt.title("Center of pressure and center of gravity position vs time", fontdict=font)
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+
+
+def plot_center_of_pressure_vs_angle_of_attack(merged_df: pd.DataFrame, aoa_df: pd.DataFrame):
+    """
+    Plot the center of pressure position vs angle of attack.
+
+    Parameters:
+    merged_df (pd.DataFrame): A DataFrame containing the time and center of pressure data.
+    aoa_df (pd.DataFrame): A DataFrame containing the time and angle of attack data.
+    """
+    font = {'family': 'serif',
+    'color':  'black',
+    'size': 12}
+    reshaped_aoa_df = aoa_df.iloc[:250, :]
+    merged_cp_and_mach_df_reshaped_to_250 = merged_df.groupby(merged_df.index // 4).mean()
+    merged_cp_and_mach_df_reshaped_to_250.head()
+    merged_cop_and_aoa_df = pd.merge_asof(
+        reshaped_aoa_df, 
+        merged_cp_and_mach_df_reshaped_to_250, 
+        left_on='Time (s)', 
+        right_on='Time', 
+        direction='nearest'
+    )
+    x_aoa = np.array(merged_cop_and_aoa_df["Angle of Attack (°)"])
+    y_cop = np.array(merged_cop_and_aoa_df["Scalar"])
+    plt.plot(x_aoa, y_cop)
+    plt.xlabel("Angle of attack (deg)", fontdict=font)
+    plt.ylabel("Center of pressure position (m)", fontdict=font)
+    plt.title("Center of pressure position vs angle of attack", fontdict=font)
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
